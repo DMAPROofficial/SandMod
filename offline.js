d767dd1805d8d2d20a9f100d595ca03b805456e7
DMAPROofficial <86569051+DMAPROofficial@users.noreply.github.com>
2021-06-27 20:56:20 +0300

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,28 @@
+﻿{
+	"version": 1624807397,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/odin-sheet0.png",
+		"images/buttonplay-sheet0.png",
+		"images/background.png",
+		"images/platforma-sheet0.png",
+		"images/tiledbackground.png",
+		"images/player-sheet0.png",
+		"images/buttonsand-sheet0.png",
+		"images/pravo-sheet0.png",
+		"images/levo-sheet0.png",
+		"images/jump-sheet0.png",
+		"images/bloksand-sheet0.png",
+		"images/delet-sheet0.png",
+		"images/pam1-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
